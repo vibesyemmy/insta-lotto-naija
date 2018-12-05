7d5bac5a12ac8d1539f9af7d68baf5748b585846
Thompson Edolo <verygreenboi@gmail.com>
2018-12-05 22:11:16 +0100

    This is a test of the build script

---
+++ build-ui.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+build_user_ui () {
+    echo Build user ui
+    cd lotto-front-main/lotto-front && ng b --prod user-front
+    cd ../../
+}
+
+build_admin_ui() {
+    echo Build admin ui
+    echo Build user ui
+    cd lotto-front-main/lotto-front && ng b --prod admin-front
+    cd ../../
+}
+
+commit() {
+    echo Enter commit message
+    read message
+    git add .
+    echo $message | git commit -F -
+}
+
+tag() {
+    echo tag
+    echo Enter commit tag
+    read tag_message
+    echo $tag_message | git commit -F -
+
+    echo Do you want want to deploy this tag?
+    read doTag
+
+    if [$doTag = y]
+    then
+        deploy $doTag
+    fi 
+}
+
+deploy() {
+    echo deploy $1
+    git push origin $1
+}
+
+echo =====================================
+echo = Do you want to build the user ui? =
+echo =             y/n                   =
+echo =====================================
+
+read print_user_ui
+
+if [ $print_user_ui = y ]
+then
+    build_user_ui
+fi
+
+echo ======================================
+echo = Do you want to build the admin ui? =
+echo =             y/n                    =
+echo ======================================
+
+read print_admin_ui
+
+if [ $print_admin_ui = y ]
+then
+    build_admin_ui
+fi
+
+echo ======================================
+echo = Do you commit all files?           =
+echo =             y/n                    =
+echo ======================================
+
+read print_commit
+
+if [ $print_commit = y ]
+then
+    commit
+fi
