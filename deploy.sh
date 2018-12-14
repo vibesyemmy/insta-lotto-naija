a608f4b9dc14e51663b847b9ab2a3237043c00f6
Thompson Edolo <verygreenboi@gmail.com>
2018-12-14 03:27:23 +0100

    Hardcode docker username

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+set -e
+
+echo "mrsmith9ja"
+
 # Build main API
-cd lotto_api && docker build -t $DOCKER_USER/lotto-main-api:$TAG -t $DOCKER_USER/lotto-main-api:latest . && cd ..
+cd lotto_api && docker build -t mrsmith9ja/lotto-main-api:$TAG -t mrsmith9ja/lotto-main-api:latest . && cd ..
 # Build main Live API
-cd lotto-live-server && docker build -t $DOCKER_USER/lotto-live-api:$TAG -t $DOCKER_USER/lotto-live-api:latest . && cd ..
+cd lotto-live-server && docker build -t mrsmith9ja/lotto-live-api:$TAG -t mrsmith9ja/lotto-live-api:latest . && cd ..
 # Build main User front end
-cd lotto-front-main && docker build -t $DOCKER_USER/lotto-user-ui:$TAG -t $DOCKER_USER/lotto-user-ui:latest  . -f Dockerfile.user.front && cd ..
+cd lotto-front-main && docker build -t mrsmith9ja/lotto-user-ui:$TAG -t mrsmith9ja/lotto-user-ui:latest  . -f Dockerfile.user.front && cd ..
 
 # Push images to docker hub
 
-docker push $DOCKER_USER/lotto-main-api:$TAG
-docker push $DOCKER_USER/lotto-main-api:latest
+docker push mrsmith9ja/lotto-main-api:$TAG
+docker push mrsmith9ja/lotto-main-api:latest
 
-docker push $DOCKER_USER/lotto-live-api:$TAG
-docker push $DOCKER_USER/lotto-live-api:latest
+docker push mrsmith9ja/lotto-live-api:$TAG
+docker push mrsmith9ja/lotto-live-api:latest
 
-docker push $DOCKER_USER/lotto-user-ui:$TAG
-docker push $DOCKER_USER/lotto-user-ui:latest
+docker push mrsmith9ja/lotto-user-ui:$TAG
+docker push mrsmith9ja/lotto-user-ui:latest
 
 kubectl apply -f k8s
-kubectl set image deployments/lotto-main-deployment main-server=$DOCKER_USER/lotto-main-api:$TAG
-kubectl set image deployments/lotto-live-api-deployment live-query-server=$DOCKER_USER/lotto-live-api:$TAG
-kubectl set image deployments/user-ui-deployment client=$DOCKER_USER/lotto-user-ui:$TAG
+kubectl set image deployments/lotto-main-deployment main-server=mrsmith9ja/lotto-main-api:$TAG
+kubectl set image deployments/lotto-live-api-deployment live-query-server=mrsmith9ja/lotto-live-api:$TAG
+kubectl set image deployments/user-ui-deployment client=mrsmith9ja/lotto-user-ui:$TAG
