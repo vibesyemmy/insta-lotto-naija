f7ddc6d14ab039a0d1c0f67406ae52e313ca3a9e
Thompson Edolo <verygreenboi@gmail.com>
2018-12-20 23:13:29 +0100

    Add landing to arch

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ cd lotto_api && docker build -t mrsmith9ja/lotto-main-api:$TAG -t mrsmith9ja/lot
 cd lotto-live-server && docker build -t mrsmith9ja/lotto-live-api:$TAG -t mrsmith9ja/lotto-live-api:latest . && cd ..
 # Build main User front end
 cd lotto-front-main && docker build -t mrsmith9ja/lotto-user-ui:$TAG -t mrsmith9ja/lotto-user-ui:latest  . -f Dockerfile.user.front && cd ..
+# Build landing page
+cd lotto-landing && docker build -t mrsmith9ja/lotto-landing:$TAG -t mrsmith9ja/lotto-landing:latest . && cd ..
 
 # Push images to docker hub
 
@@ -21,6 +23,9 @@ docker push mrsmith9ja/lotto-live-api:latest
 
 docker push mrsmith9ja/lotto-user-ui:$TAG
 docker push mrsmith9ja/lotto-user-ui:latest
+
+docker push mrsmith9ja/lotto-landing:$TAG
+docker push mrsmith9ja/lotto-landing:latest
 
 kubectl apply -f k8s
 kubectl set image deployments/lotto-main-deployment main-server=mrsmith9ja/lotto-main-api:$TAG
