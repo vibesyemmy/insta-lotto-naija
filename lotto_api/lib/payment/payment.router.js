f157e575b7feb660f2d480d7b1dfbf6f17a40bac
Thompson Edolo <verygreenboi@gmail.com>
2018-12-31 03:27:04 +0100

    Change payment webhook endpoint

--- lotto_api/lib/payment/payment.router.js
+++ lotto_api/lib/payment/payment.router.js
@@ -8,7 +8,7 @@ router.get('/', (_, res) => {
     res.json({message: "This is the payment end point"});
 });
 // router.get('/:id', Controller.get);
-// router.post('/', Controller.create);
+router.post('/', Controller.webhook);
 // router.put('/:id', Controller.update);
 // router.delete('/:id', Controller.destroy);
 router.post('/webhook', Controller.webhook);
