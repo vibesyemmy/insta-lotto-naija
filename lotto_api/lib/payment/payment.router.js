18ee013004c0fdbe87786e62e7469615d65211a2
Thompson Edolo <verygreenboi@gmail.com>
2018-12-31 15:59:40 +0100

    Debug payment webhook endpoint

--- lotto_api/lib/payment/payment.router.js
+++ lotto_api/lib/payment/payment.router.js
@@ -25,27 +25,29 @@ router.post('/', async (req, res) => {
     const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(event)).digest('hex');
 
     if (hash == req.headers['x-paystack-signature']) {
+        const opts = {
+            event: event.event,
+            id: event.data.id,
+            domain: event.data.domain,
+            status: event.data.status,
+            reference: event.data.reference,
+            amount: event.data.amount,
+            gateway_response: event.data.gateway_response,
+            // paid_at: {
+            //     "__type": "Date",
+            //     "iso": event.data.paid_at
+            // },
+            // charge_created_at: {
+            //     "__type": "Date",
+            //     "iso": event.data.created_at
+            // },
+            channel: event.data.channel,
+            currency: event.data.currency,
+            ip_address: event.data.ip_address
+        };
+        console.info("Request options", opts);
         try {
-            await instance.post(`http://localhost:${PORT}/${mountPath}/classes/Payment`,{
-                event: event.event,
-                id: event.data.id,
-                domain: event.data.domain,
-                status: event.data.status,
-                reference: event.data.reference,
-                amount: event.data.amount,
-                gateway_response: event.data.gateway_response,
-                paid_at: {
-                    "__type": "Date",
-                    "iso": event.data.paid_at
-                },
-                charge_created_at: {
-                    "__type": "Date",
-                    "iso": event.data.created_at
-                },
-                channel: event.data.channel,
-                currency: event.data.currency,
-                ip_address: event.data.ip_address
-            });
+            await instance.post(`http://localhost:${PORT}/${mountPath}/classes/Payment`, opts);
             res.sendStatus(200);
         } catch (error) {
             console.error(error.message);
