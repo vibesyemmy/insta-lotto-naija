5b8c30c1798ded0e1fba416fff77d3013424855a
Thompson Edolo <verygreenboi@gmail.com>
2019-01-22 12:27:22 +0100

    Wire transfer credits button in wallet

---
+++ lotto-front-main/lotto-front/libs/wallet/src/lib/transfer-funds/transfer-funds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lotto-front-transfer-funds',
+  templateUrl: './transfer-funds.component.html',
+  styleUrls: ['./transfer-funds.component.css']
+})
+export class TransferFundsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lotto-front-main/lotto-front/libs/wallet/src/lib/wallet.module.ts
+++ lotto-front-main/lotto-front/libs/wallet/src/lib/wallet.module.ts
@@ -4,15 +4,17 @@ import { RouterModule } from '@angular/router';
 import { ButtonsModule, WavesModule, CardsFreeModule } from 'angular-bootstrap-md'
 import { WalletComponent } from './wallet/wallet.component';
 import { AddFundsComponent } from './add-funds/add-funds.component';
+import { TransferFundsComponent } from './transfer-funds/transfer-funds.component';
 
 @NgModule({
   imports: [
     CommonModule, ButtonsModule, WavesModule, CardsFreeModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: WalletComponent },
-      { path: 'addFunds', component: AddFundsComponent }
+      { path: 'addFunds', component: AddFundsComponent },
+      { path: 'transferFunds', component: TransferFundsComponent }
     ])
   ],
-  declarations: [WalletComponent, AddFundsComponent]
+  declarations: [WalletComponent, AddFundsComponent, TransferFundsComponent]
 })
 export class WalletModule { }
