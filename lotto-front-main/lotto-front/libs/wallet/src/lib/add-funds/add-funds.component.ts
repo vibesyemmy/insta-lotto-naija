53197cbd694b0ac42277669222e53b43c2a09bb6
Thompson Edolo <verygreenboi@gmail.com>
2019-01-22 12:22:56 +0100

    Wire add funds button in wallet

---
+++ lotto-front-main/lotto-front/libs/wallet/src/lib/add-funds/add-funds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lotto-front-add-funds',
+  templateUrl: './add-funds.component.html',
+  styleUrls: ['./add-funds.component.css']
+})
+export class AddFundsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lotto-front-main/lotto-front/libs/wallet/src/lib/wallet.module.ts
+++ lotto-front-main/lotto-front/libs/wallet/src/lib/wallet.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonsModule, WavesModule, CardsFreeModule } from 'angular-bootstrap-md'
 import { WalletComponent } from './wallet/wallet.component';
+import { AddFundsComponent } from './add-funds/add-funds.component';
 
 @NgModule({
   imports: [
     CommonModule, ButtonsModule, WavesModule, CardsFreeModule,
     RouterModule.forChild([
-      { path: '', pathMatch: 'full', component: WalletComponent }
+      { path: '', pathMatch: 'full', component: WalletComponent },
+      { path: 'addFunds', component: AddFundsComponent }
     ])
   ],
-  declarations: [WalletComponent]
+  declarations: [WalletComponent, AddFundsComponent]
 })
 export class WalletModule { }
