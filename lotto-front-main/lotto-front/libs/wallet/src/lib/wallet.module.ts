245cac1aa9221ede9d5421336a5bd912778c3b6e
Thompson Edolo <verygreenboi@gmail.com>
2019-01-22 11:50:25 +0100

    Start wallet page

--- lotto-front-main/lotto-front/libs/wallet/src/lib/wallet.module.ts
+++ lotto-front-main/lotto-front/libs/wallet/src/lib/wallet.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ButtonsModule, WavesModule, CardsFreeModule } from 'angular-bootstrap-md'
 import { WalletComponent } from './wallet/wallet.component';
 
 @NgModule({
   imports: [
-    CommonModule,
+    CommonModule, ButtonsModule, WavesModule, CardsFreeModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: WalletComponent }
     ])
--- lotto-front-main/lotto-front/libs/wallet/src/lib/wallet/wallet.component.ts
+++ lotto-front-main/lotto-front/libs/wallet/src/lib/wallet/wallet.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
+  balance = 5000;
+
   constructor() { }
 
   ngOnInit() {
